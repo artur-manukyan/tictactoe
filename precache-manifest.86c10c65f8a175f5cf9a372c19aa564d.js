7dd890404215afe1ffd84eac8b36cdbdc4094e8c
Artur <manukyan.personal@gmail.com>
2020-10-22 02:17:59 +0400

    Updates

---
+++ precache-manifest.86c10c65f8a175f5cf9a372c19aa564d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1425c0f1909cf87c825d4f2968bd16d2",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "737b91e12c9378e6e4a6",
+    "url": "/tictactoe/static/css/main.8e56c59f.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/tictactoe/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "737b91e12c9378e6e4a6",
+    "url": "/tictactoe/static/js/main.f02188ca.chunk.js"
+  },
+  {
+    "revision": "a943afe6c89606c3dd45",
+    "url": "/tictactoe/static/js/runtime-main.c2a2dfd1.js"
+  }
+]);
